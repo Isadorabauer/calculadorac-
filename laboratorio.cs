91db332fce0db4a01ff135865a27d4e2b9c34a06
Isadorabauer <isadora.bauer@rede.ulbra.br>
2024-08-21 15:14:27 -0300

    calculadora

---
+++ laboratorio.cs
@@ -0,0 +1,26 @@
+int x, y;
+double divisao;
+Console.WriteLine("Digitar o valor de X:");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o valor de y");
+Convert. ToInt32(Console.ReadLine());
+
+int x, y;
+double multiplicacao;
+Console.WriteLine("Digitar o valor de X:");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o valor de y");
+Convert. ToInt32(Console.ReadLine());
+
+int x, y;
+double divisao;
+Console.WriteLine("Digitar o valor de X:");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o valor de y");
+Convert. ToInt32(Console.ReadLine());
+
+divisao = x / y;
+multiplicacao = x * y;
+adicao = x + y;
+subtracao = x - y;
+
